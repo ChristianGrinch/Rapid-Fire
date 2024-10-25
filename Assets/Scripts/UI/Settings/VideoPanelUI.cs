10854d2b7547d51cfee6dc92ab9d1b41c9df34ff
ChristianGrinch <christiangingrich12707@Gmail.com>
2024-10-25 10:36:57 -0700

    fixed video mode

--- Assets/Scripts/UI/Settings/VideoPanelUI.cs
+++ Assets/Scripts/UI/Settings/VideoPanelUI.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class VideoPanelUI : MonoBehaviour
 {
@@ -18,8 +20,49 @@ public class VideoPanelUI : MonoBehaviour
     }
     [Header("Panel")]
     public GameObject videoPanel;
+	public TMP_Dropdown screenMode;
     void Start()
     {
-        
-    }
+		screenMode.onValueChanged.AddListener(ChangeScreenMode);
+		if (screenMode.options.Count == 0)
+		{
+			screenMode.options.Add(new TMP_Dropdown.OptionData("Exclusive Fullscreen"));
+			screenMode.options.Add(new TMP_Dropdown.OptionData("Fullscreen Window"));
+			screenMode.options.Add(new TMP_Dropdown.OptionData("Maximized Window"));
+			screenMode.options.Add(new TMP_Dropdown.OptionData("Windowed"));
+		}
+		//switch (Screen.fullScreenMode)
+		//{
+		//	case FullScreenMode.ExclusiveFullScreen:
+		//		screenMode.value = 0;
+		//		break;
+		//	case FullScreenMode.FullScreenWindow:
+		//		screenMode.value = 1;
+		//		break;
+		//	case FullScreenMode.MaximizedWindow:
+		//		screenMode.value = 2;
+		//		break;
+		//	case FullScreenMode.Windowed:
+		//		screenMode.value = 3;
+		//		break;
+		//}
+	}
+	private void ChangeScreenMode(int selectedIndex)
+	{
+		switch (selectedIndex)
+		{
+			case 0:
+				Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+				break;
+			case 1:
+				Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+				break;
+			case 2:
+				Screen.fullScreenMode = FullScreenMode.MaximizedWindow;
+				break;
+			case 3:
+				Screen.fullScreenMode = FullScreenMode.Windowed;
+				break;
+		}
+	}
 }
