3838fa710ac7467852e9c4f3b988067350fe7943
ChristianGrinch <christiangingrich12707@Gmail.com>
2024-11-14 11:12:08 -0800

    slot prefabs and unique instantiation
    - created prefabs for the slots. one for a normal slot, and a unique prefab for each hud slot due to position and rotation.
    - made it so that the slots are dynamically instantiated on start so its easier to modify them and work with them. helps with future expansion if needed too.

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -8,20 +8,39 @@ public class InventoryManager : MonoBehaviour
 	public int slotCount = (6 * 4) + 5; // Column x Row inventory slots plus 5 HUD slots
 	private List<GameObject> slots;
 	[Header("Game objects")]
+	public List<GameObject> HUDSlots = new(5);
+	public GameObject slotPrefab;
 	public GameObject inventory;
 	public GameObject HUD;
 
 	public Button button;
 
-	void Start()
+	public void Start()
 	{
-		InitializeSlots();
+		CreateSlots();
+		AssignSlots();
 		button.onClick.AddListener(() =>
 		{
 			slots[4].GetComponent<SlotData>().SetSlotData(3);
 		});
 	}
-	void InitializeSlots()
+	void CreateSlots()
+	{
+		// Create inventory slots
+		for (var i = 0; i < slotCount - 5; i++)
+		{
+			GameObject slot = Instantiate(slotPrefab, inventory.transform);
+			slot.name = "Slot " + i;
+		}
+
+		// Create HUD slots
+		for (var i = 0; i < 5; i++)
+		{
+			GameObject slot = Instantiate(HUDSlots[i], HUD.transform);
+			slot.name = "Slot " + (slotCount - 4 + i); // only minus 4 instead of 5 because the index starts at 0
+		}
+	}
+	void AssignSlots()
 	{
 		slots = new(slotCount);
 		foreach (Transform slot in inventory.transform)
